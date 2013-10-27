c54c77e31555fd978dd506a309810b51e38d2738
JulienBernard <pro.julienbernard@free.fr>
2013-10-27 20:51:27 +0100

    Fix a little problem of reload. When an error has been catch, add a link to return to the page with the same parameters.

--- template/footer.admin.php
+++ template/footer.admin.php
@@ -1,25 +1,25 @@
 
 	<div class="Modal" id="modalContent">
 		<div class="popup_block">
-			<a href="<?php echo $_SERVER['PHP_SELF']; ?>#noWhere" class="right">&#215;</a>
+			<a href="<?php echo $_SERVER['PHP_SELF']; if( isset($_SERVER['QUERY_STRING']) ) echo '?'.$_SERVER['QUERY_STRING']; ?>#noWhere" class="right">&#215;</a>
 			<?php
 			if( $Engine->getError() != null )
 			{
 				echo '<p class="lead">An error has been detected!</p>';
 				echo '<p>'.$Engine->getError().'</p>';
-				echo '<br /><a href="'.$_SERVER["PHP_SELF"].'" class="center">Click here to retry!</a>';
+				echo '<br /><a href="'.$_SERVER["PHP_SELF"].'?'.$_SERVER['QUERY_STRING'].'#noWhere" class="center">Click here to retry!</a>';
 			}
 			else if( $Engine->getSuccess() != null )
 			{
 				echo '<p class="lead">Success!</p>';
 				echo $Engine->getSuccess();
-				echo '<br /><a href="'.$_SERVER["PHP_SELF"].'" class="center">Click here to retry!</a>';
+				echo '<br /><a href="'.$_SERVER["PHP_SELF"].'?'.$_SERVER['QUERY_STRING'].'#noWhere" class="center">Click here to retry!</a>';
 			}
 			else if( $Engine->getInfo() != null )
 			{
 				echo '<p class="lead">Informations:</p>';
 				echo $Engine->getInfo();
-				echo '<br /><a href="'.$_SERVER["PHP_SELF"].'" class="center">Click here to retry!</a>';
+				echo '<br /><a href="'.$_SERVER["PHP_SELF"].'?'.$_SERVER['QUERY_STRING'].'#noWhere" class="center">Click here to retry!</a>';
 			}
 			?>
 		</div>
